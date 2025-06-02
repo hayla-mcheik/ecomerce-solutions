a0cd7c9ef292f22644055cdce097b08568e1f2f5
hayla-mcheik <117450554+hayla-mcheik@users.noreply.github.com>
2025-06-02 14:13:50 +0300

    first commit

--- resources/js/Components/AdminNavbar.vue
+++ resources/js/Components/AdminNavbar.vue
@@ -36,13 +36,9 @@
             </svg>
             <span class="sr-only">Toggle sidebar</span>
           </button>
-          <a href="https://flowbite.com" class="flex items-center justify-between mr-4">
-            <img
-              src="https://flowbite.s3.amazonaws.com/logo.svg"
-              class="mr-3 h-8"
-              alt="Flowbite Logo"
-            />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+          <a href="/" class="flex items-center justify-between mr-4">
+
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">NanoEssence</span>
           </a>
           <form action="#" method="GET" class="hidden md:block md:pl-2">
             <label for="topbar-search" class="sr-only">Search</label>
--- resources/js/Components/Aside.vue
+++ resources/js/Components/Aside.vue
@@ -69,7 +69,7 @@
           </li>
           <li>
             <a
-              href="/user/orders"
+              href="#"
               class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               My Orders
